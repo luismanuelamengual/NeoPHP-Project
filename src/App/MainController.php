c3f8604dc6fd5e896a9d8a13b001c0e2b5bac6d3
Luis Amengual <luis.amengual@sitrack.com>
2018-03-01 11:11:04 -0300

    cambio menor

--- src/App/MainController.php
+++ src/App/MainController.php
@@ -7,13 +7,9 @@ use NeoPHP\Routing\Routes;
 class MainController {
 
     public function main() {
-        $this->registerRoutes();
-        $this->handleRequest();
-    }
 
-    public function registerRoutes() {
         Routes::get("/", function () {
-            return create_view("welcome", ["name"=>get_current_user()]);
+            return create_view("welcome", ["name" => get_current_user()]);
         });
     }
 }
