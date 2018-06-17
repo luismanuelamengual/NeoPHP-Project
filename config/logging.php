de4c0e1fd47b49bc4ba6e5cb37affbc27782d3c1
luis <luismanuelamengual@gmail.com>
2018-06-17 15:11:51 -0300

    Cambios para 4.1

--- config/logging.php
+++ config/logging.php
@@ -17,6 +17,7 @@ return [
             "formatter" => "main_formatter",
             "filename" => get_app()->storagePath() . DIRECTORY_SEPARATOR . "logs" . DIRECTORY_SEPARATOR . "{date}",
             "maxFiles" => 30,
+            "filePermission" => 777,
             "set_filename_format" => ["{date}.log", "Y-m-d"]
         ]
     ],
